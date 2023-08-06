028b646e9ffc135357f75ca468eef86cbaf0bb26
LeoArs06 <leoars03@gmail.com>
2023-08-07 00:25:04 +0200

    refact and fix

--- src/main/java/ares06/inactivityshutdown/Inactivityshutdown.java
+++ src/main/java/ares06/inactivityshutdown/Inactivityshutdown.java
@@ -1,53 +1,115 @@
 package ares06.inactivityshutdown;
 
-import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.logging.Logger;
+import java.util.Timer;
+import java.util.TimerTask;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.entity.Player;
 
 public final class Inactivityshutdown extends JavaPlugin {
-    private int inactivityTimeout; // in seconds
-    private int taskId;
-    private boolean pluginEnabled;
+    private Logger logger;
+    private int idleWaitTime;
+    private Timer idleTimer;
+
+    private final Listener playerQuitListener = new PlayerQuitListener(this);
+    private final Listener playerJoinListener = new PlayerJoinListener(this);
 
     @Override
     public void onEnable() {
-        saveDefaultConfig(); // Save the default configuration file if it doesn't exist
+        logger = getLogger();
+        saveDefaultConfig();
+        idleWaitTime = Math.max(0, getConfig().getInt("idle_wait_time"));
+        logger.info("This server is running IdleShutdown: It will stop after " + idleWaitTime + " seconds with no player online.");
 
-        FileConfiguration config = getConfig();
-        inactivityTimeout = config.getInt("inactivityTimeout", 1800); // Get value from the configuration file, default 1800 seconds (30 minutes)
-        pluginEnabled = config.getBoolean("Enabled", false); // Get the plugin's state from the configuration file, default disabled
+        registerEvent(playerQuitListener);
 
-        if (pluginEnabled) {
-            getServer().getPluginManager().registerEvents(new PlayerActivityListener(), this);
-            startInactivityCheckTask();
+        if (noPlayerOnline()) {
+            logger.fine("There are no players online!");
+            startIdleTimer();
         }
     }
 
     @Override
     public void onDisable() {
-        Bukkit.getScheduler().cancelTask(taskId);
+        cancelIdleTimer();
+        unregisterAllListeners();
     }
 
-    public int getInactivityTimeout() {
-        return inactivityTimeout;
+    public void onTimerExpired() {
+        logger.fine("Timer expired!");
+
+        if (noPlayerOnline()) {
+            logAndShutdown("No players online, shutting down");
+        } else {
+            logger.warning("A player has come online and we have not been notified! Something is wrong.");
+        }
     }
 
-    private void startInactivityCheckTask() {
-        taskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
-            long currentTime = System.currentTimeMillis();
-            if (pluginEnabled) {
-                boolean activePlayersFound = false;
-                for (Player player : Bukkit.getOnlinePlayers()) {
-                    if (player.getLastPlayed() + (inactivityTimeout * 1000) >= currentTime) {
-                        activePlayersFound = true;
-                        break;
-                    }
+    void onPlayerQuit() {
+        logger.fine("A player has quit!");
+
+        if (lastPlayerOnline()) {
+            logger.info("The last player is leaving!");
+            startIdleTimer();
+        }
+    }
+
+    void onPlayerJoin() {
+        logger.fine("A player has joined!");
+
+        unregisterAllListeners();
+        cancelIdleTimer();
+    }
+
+    private void registerEvent(Listener listener) {
+        getServer().getPluginManager().registerEvents(listener, this);
+    }
+
+    private boolean lastPlayerOnline() {
+        return getServer().getOnlinePlayers().size() <= 1;
+    }
+
+    private boolean noPlayerOnline() {
+        return getServer().getOnlinePlayers().isEmpty();
+    }
+
+    private void cancelIdleTimer() {
+        if (idleTimer != null) {
+            logger.finer("Cancelling timer...");
+            idleTimer.cancel();
+        }
+    }
+
+    private void unregisterAllListeners() {
+        logger.finer("Unregistering listeners...");
+        HandlerList.unregisterAll(this);
+    }
+
+    private void logAndShutdown(String message) {
+        logger.info(message);
+        unregisterAllListeners();
+        getServer().shutdown();
+    }
+
+    private void startIdleTimer() {
+        if (idleWaitTime == 0) {
+            logger.fine("idle_wait_time is 0, not scheduling timer!");
+            onTimerExpired();
+        } else {
+            registerEvent(playerJoinListener);
+
+            logger.finer("Creating and scheduling timer...");
+            idleTimer = new Timer();
+            TimerTask idleTimerTask = new TimerTask() {
+                @Override
+                public void run() {
+                    onTimerExpired();
                 }
-                if (!activePlayersFound) {
-                    Bukkit.shutdown(); // Shutdown the server only if no active players were found
-                }
-            }
-        }, 0L, 20L * 60L); // Check every minute
+            };
+            idleTimer.schedule(idleTimerTask, idleWaitTime * 1000);
+        }
     }
 }
---
+++ src/main/java/ares06/inactivityshutdown/PlayerQuitListener.java
@@ -0,0 +1,24 @@
+package ares06.inactivityshutdown;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerQuitListener implements Listener {
+    Inactivityshutdown mainPlugin;
+
+    public PlayerQuitListener(Inactivityshutdown mainPlugin) {
+        this.mainPlugin = mainPlugin;
+    }
+
+    // Set lowest event priority to execute as late as possible. The idea being
+    // that other plugins might modify player events to hide "non-player" joins,
+    // kick players immediately or whatever, and we want to see the end result
+    // of all that, not the intermediate steps.
+    // This is just a guess how to behave, and might be totally wrong. Thus, it
+    // might also be changed in the future.
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        this.mainPlugin.onPlayerQuit();
+    }
+}
